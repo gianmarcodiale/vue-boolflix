f0aba2f4e33766f8e2a6205899c0d5f6b93b812f
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-09 11:52:35 +0200

    initial commit

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <input type="search" name="search" id="search" />
+    <button type="submit">Cerca</button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SiteHeader",
+};
+</script>
+
+<style lang='scss' scoped></style>
