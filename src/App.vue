99faba5a20a79ece45a055c2f668f23ddaefce2f
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-10 13:19:02 +0200

    add stars for vote and logo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <header>
-      <input type="search" name="search" id="search" v-model="search" />
-      <button
-        @click="
-          callMoviesApi();
-          callSeriesApi();
-        "
-      >
-        Cerca
-      </button>
+    <!-- SiteHeader -->
+    <header class="d-flex justify-content-between align-items-center">
+      <div class="logo text-center p-3">
+        <img src="@/assets/img/boolflix_logo.png" alt="" />
+      </div>
+      <div class="search-commands text-center">
+        <input type="search" name="search" id="search" v-model="search" />
+        <button
+          @click="
+            callMoviesApi();
+            callSeriesApi();
+          "
+        >
+          Cerca
+        </button>
+      </div>
+    </header>
+    <!-- /SiteHeader -->
+    <!-- SiteMain -->
+    <main>
       <div class="container">
-        <div class="row row-cols-5">
+        <div class="row row-cols-5 pt-4">
           <div class="col" v-for="movie in movies" :key="movie.id">
-            <div class="movie-card">
+            <div class="movie-card text-white">
               <img
                 src="@/assets/img/default.jpeg"
                 alt=""
@@ -31,11 +41,15 @@
               <h2>{{ movie.title }}</h2>
               <h4>{{ movie.original_title }}</h4>
               <div>Lingua originale: <img :src="language(movie)" alt="" /></div>
-              <div>Media voto: {{ movie.vote_average }}</div>
+              <rate
+                :length="5"
+                :value="Math.ceil(movie.vote_average / 2)"
+                :readonly="true"
+              />
             </div>
           </div>
           <div class="col" v-for="serie in tvSeries" :key="serie.id">
-            <div class="serie-card">
+            <div class="serie-card text-white">
               <img
                 src="@/assets/img/default.jpeg"
                 alt=""
@@ -53,12 +67,17 @@
               <h2>{{ serie.name }}</h2>
               <h4>{{ serie.original_name }}</h4>
               <div>Lingua originale: <img :src="language(serie)" alt="" /></div>
-              <div>Media voto: {{ serie.vote_average }}</div>
+              <rate
+                :length="5"
+                :value="Math.ceil(serie.vote_average / 2)"
+                :readonly="true"
+              />
             </div>
           </div>
         </div>
       </div>
-    </header>
+    </main>
+    <!-- /SiteMain -->
   </div>
 </template>
 
@@ -124,6 +143,27 @@ export default {
 
 <style lang="scss">
 @import "@/assets/scss/style.scss";
+
+header {
+  background-color: black;
+
+  .search-commands {
+    padding: 2rem;
+
+    input {
+      border-radius: 0.5rem;
+      border: none;
+      margin-right: 0.25rem;
+    }
+
+    button {
+      background-color: crimson;
+      border-radius: 0.5rem;
+      color: white;
+      border: none;
+    }
+  }
+}
 
 img.default {
   border: 1px solid rgba(187, 187, 187, 0.543);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import rate from 'vue-rate'
+import 'vue-rate/dist/vue-rate.css'
+
+Vue.use(rate)
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
